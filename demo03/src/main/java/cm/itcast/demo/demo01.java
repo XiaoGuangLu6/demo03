2d8d7a4c5867f33c94a4d840848f6f30b690c451
XiaoGuangLu6 <1164653576@qq.com>
2019-12-22 19:10:02 +0800

    1

---
+++ demo03/src/main/java/cm/itcast/demo/demo01.java
@@ -0,0 +1,17 @@
+package cm.itcast.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/demo")
+public class demo01 {
+
+
+    @RequestMapping("/demo01")
+    @ResponseBody
+    public String demo01(){
+        return "springboot";
+    }
+}
---
+++ demo03/src/main/java/cm/itcast/demoApplication.java
@@ -0,0 +1,13 @@
+package cm.itcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class demoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(demoApplication.class,args);
+    }
+}
